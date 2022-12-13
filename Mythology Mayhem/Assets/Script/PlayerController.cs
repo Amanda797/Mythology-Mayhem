1ec0f9e4d9cd84c361ac03b0415aeba743f35f3a
Dan <sidato83@gmail.com>
2022-12-12 22:11:21 -0700

    Added player movement script and animations
    Player can now move, and has idle, walk, and run animations with animator transitions.

---
+++ Mythology Mayhem/Assets/Script/PlayerController.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    [Header("Player Movement")]
+    [SerializeField] private float walkSpeed = 200f;
+    [SerializeField] private float runSpeed = 300f;
+
+    [Header("Player Animation")]
+    [SerializeField] private Animator anim;
+
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+
+    private float xMovement;
+    private float yMovement;
+    private bool isRunning = false;
+    #endregion Variables
+
+
+    #region Unity Methods
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        FlipPlayerSpriteWithMoveDirection();
+        AnimatePlayer();
+    }
+
+    private void FlipPlayerSpriteWithMoveDirection()
+    {
+        if (xMovement > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (xMovement < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isRunning)
+        {
+            MovePlayer(walkSpeed);
+        }
+        else
+        {
+            MovePlayer(runSpeed);
+        }
+    }
+    #endregion Unity Methods
+
+
+    #region Self-defined Methods
+    private bool GetInput()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        return xMovement != 0;
+    }
+
+    private void MovePlayer(float speed)
+    {
+        rb2d.velocity = new Vector2((xMovement * speed) * Time.deltaTime, rb2d.velocity.y);
+    }
+
+    private void AnimatePlayer()
+    {
+        anim.SetBool("IsWalking", GetInput());
+        anim.SetBool("IsRunning", isRunning);
+    }
+    #endregion Self-defined Methods
+}
