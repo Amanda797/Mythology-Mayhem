191c040c24c3562ba4e1edee652fe4e8604b50d4
Dan <sidato83@gmail.com>
2022-12-15 20:20:44 -0700

    Added attack trigger for mouse
    When Greek Boy enters mouse trigger, mouse run animation will activate.

---
+++ Mythology Mayhem/Assets/Scripts/MouseAI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class MouseAI : MonoBehaviour
+{
+    [Header("Mouse Movement")]
+    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float runSpeed = 15f;
+
+    [Header("Attack Activation")]
+    [SerializeField] private BoxCollider2D soundTrigger;
+    [SerializeField] private GameObject attackTarget;
+
+    [Header("Mouse Animations")]
+    [SerializeField] private Animator mouseAnim;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttackPlayer();
+    }
+
+    void AttackPlayer()
+    {
+        bool attackTrigger = soundTrigger.IsTouching(attackTarget.GetComponent<Collider2D>());
+        if (attackTrigger)
+        {
+            Debug.Log("ATTACK GREEK BOY!");
+        }
+        mouseAnim.SetBool("IsAttacking", attackTrigger);
+    }
+}
