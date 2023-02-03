8aaa689f7c6c7251185e05ff84fa5ab21c2f07a2
Andrew Bondurant <stdrew190@gmail.com>
2023-02-03 17:09:23 -0500

    Updated lever

--- Mythology Mayhem/Assets/TwoDLever.cs
+++ Mythology Mayhem/Assets/TwoDLever.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TwoDLever : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+
     public bool entered = false;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,13 @@ public class TwoDLever : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (entered)
+        {
+            if (Input.GetKeyDown(KeyCode.E)) 
+            {
+                anim.SetTrigger("Pulled");
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
