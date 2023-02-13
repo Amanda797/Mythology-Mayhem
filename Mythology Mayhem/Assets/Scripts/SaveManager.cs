5181ed10e9493b09e050c4663a23d00dca349292
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2023-02-13 06:05:03 -0500

    Pause Menu is fleshed out
    Still needs to pause the game

---
+++ Mythology Mayhem/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Save() {
+        print("Saved Game");
+    }
+
+    public void Load() {
+        print("Loaded Game");
+    }
+}
