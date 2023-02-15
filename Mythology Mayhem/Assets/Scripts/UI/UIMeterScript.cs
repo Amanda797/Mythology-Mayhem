ccd48e154891ff9e07c4f3700978794be20e42d3
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2023-02-14 22:54:44 -0500

    Updates to 2D testing scene
    Testing Health UI
    Testing pause menu
    Added saving scripts (to delete perhaps, Drew has a save system that we can use)

--- Mythology Mayhem/Assets/Scripts/UI/UIMeterScript.cs
+++ Mythology Mayhem/Assets/Scripts/UI/UIMeterScript.cs
@@ -6,20 +6,23 @@ using TMPro;
 public class UIMeterScript : MonoBehaviour
 {
     // The meter's background transform
-    public RectTransform background;
+    [SerializeField] RectTransform background;
     // The meter's foreground transform (the image that should be scalable)
-    public RectTransform foreground;
+    [SerializeField] RectTransform foreground;
     // The width of the background's transform, as big as the background and foreground can get
     float maxMeter;
     // The width of the foreground's transform currently, its current percentage of fullness
     float currentMeter;
-    // The width in terms of 100
+    // The base that the meter is set in (ex 100/100, playerCurrHealth/playerMaxHealth, etc.)
+    float maxBase;
+    // The width in terms of maxBase
     float currentMeterWeighted;
-    public TextMeshProUGUI currentMeterNumber;
+    [SerializeField] TextMeshProUGUI currentMeterNumber;
     
-    int testStep = 1;
-    float timer = 0f;
-    float duration = 3f;
+    // Testing variables
+    //int testStep = 1;
+    //float timer = 0f;
+    //float duration = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,34 +37,37 @@ public class UIMeterScript : MonoBehaviour
 
         // set the scalable meter, currentMeter, equal to the starting width, maxMeter
         currentMeter = maxMeter;
+
+        // set max base to a default until the scale is set by player ui
+        maxBase = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Testing functionality. Unhide: testStep, timer, and duration.
-
+        /*
         timer += Time.deltaTime;
-
+        
         if(timer > duration) {
             switch(testStep) {
-                case 1: DecreaseMeter(40); break;
-                case 2: IncreaseMeter(20); break;
-                case 3: DecreaseMeter(90); break;
-                case 4: IncreaseMeter(100.9f); break;
-                case 5: DecreaseMeter(150); break;
-                case 6: IncreaseMeter(50); break;
-                case 7: DecreaseMeter(-30); break;
-                case 8: IncreaseMeter(-70); break;
-                case 9: DecreaseMeter(13.38f); break;
-                case 10: IncreaseMeter(45.9f); break;
+                case 1: UpdateMeter(40); break;
+                case 2: UpdateMeter(20); break;
+                case 3: UpdateMeter(90); break;
+                case 4: UpdateMeter(100.9f); break;
+                case 5: UpdateMeter(150); break;
+                case 6: UpdateMeter(50); break;
+                case 7: UpdateMeter(-30); break;
+                case 8: UpdateMeter(-70); break;
+                case 9: UpdateMeter(13.38f); break;
+                case 10: UpdateMeter(45.9f); break;
                 default: break;
             }
             testStep++; 
             timer = 0f;
         }
+        */
         
-
         // Constrain the meter to the mins and max's of the transforms
         if(currentMeter <= 0) {
             currentMeter = 0;
@@ -69,29 +75,27 @@ public class UIMeterScript : MonoBehaviour
             currentMeter = maxMeter;
         }
 
-        // reset foreground size according to updated value of currentMeter
+        // Reset the foreground size according to the updated value of currentMeter
         foreground.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, currentMeter);
         foreground.ForceUpdateRectTransforms();
 
         // Set the text panel's number
-        currentMeterWeighted = (currentMeter * 100) / maxMeter;
+        currentMeterWeighted = (currentMeter * maxBase) / maxMeter;
         currentMeterNumber.text = currentMeterWeighted + "";
     }
 
-    public void IncreaseMeter(float amount) {
-        float newLevel = currentMeter + ((Mathf.Abs(amount) * maxMeter) / 100);
-        currentMeter = newLevel;
-        // newLevel = {ex.3452} + ((amount * maxMeter) / 100);
+    
+
+    // Update Meter updates the meter's current level to a new amount (that is weighted).
+    public void UpdateMeter(float amount) {
+        currentMeter = ((Mathf.Abs(amount) * maxMeter) / maxBase);
     }
 
-    public void DecreaseMeter(float amount) {
-        
-        float newLevel = currentMeter - ((Mathf.Abs(amount) * maxMeter) / 100);
-        currentMeter = newLevel;
-        // newLevel = {ex.3452} - ((amount * maxMeter) / 100);
-    }
-
-    public void GetHealth() {
-        
+    // Set Meter updates the meter's scale based on the maximum value possible and the current amount of that possibility used.
+    // setMax - maximum limit
+    // setCurr - current amount
+    public void SetMeter(float setMax, float setCurr) {
+        maxBase = setMax;
+        currentMeter = ((Mathf.Abs(setCurr) * maxMeter) / maxBase);
     }
 }
