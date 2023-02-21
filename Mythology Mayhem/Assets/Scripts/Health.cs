a740a1b221bc6dddde7d7ad4f2896d6c7c1d95f7
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2023-02-21 02:56:46 -0500

    Animated 3D bulls in Labyrinth, AI Navigation

--- Mythology Mayhem/Assets/Scripts/Health.cs
+++ Mythology Mayhem/Assets/Scripts/Health.cs
@@ -5,7 +5,28 @@ public class Health : MonoBehaviour
     [SerializeField] private float MaxHealth;
     [SerializeField] private float health;
     [SerializeField] Behaviour[] components;
+    [SerializeField] GameObject mainObject; // Parent Self
 
+    [Header("Animation")]
+    [SerializeField] Animator anim;
+    [SerializeField] string hurtAnim;
+    [SerializeField] string deathAnim;
+    [SerializeField] string healAnim;
+
+    void Start()
+    {
+        health = MaxHealth;
+    }
+
+    void Update() {
+        // test
+        health -= 1f * Time.deltaTime;
+        //print(health);
+
+        if(health <= 0) {
+            Death();
+        }
+    }
 
     public void SetHealth(float h) {
         health += h;
@@ -16,29 +37,23 @@ public class Health : MonoBehaviour
     }
 
     public void TakeDamage(float d) {
-        health -= d;
+        anim.SetTrigger(hurtAnim);
+        SetHealth(-d);
     }
 
     public void Heal(float h) {
-        health += h;
-        //anim.SetTrigger("hurt"); //all actors that use this health script should have an animation clip called hurt in their animation controller
-    }
-
-    void Start()
-    {
-        health = MaxHealth;
-    }
-
-    void Update() {
-        //health -= 1f * Time.deltaTime;
-        //print(health);
+        anim.SetTrigger(healAnim);
+        SetHealth(h);
     }
 
     public void Death() {
-        
+        anim.SetTrigger(deathAnim);       
+        //this? 
         foreach (Behaviour component in components) {
             component.enabled = false;
         }
+        //or this??
+        Destroy(mainObject, 3f);
     }
 
 }
