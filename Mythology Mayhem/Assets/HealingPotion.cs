5ed423afbc41358b9482a0fc334e85361e991701
TheGrim159 <stdrew190@gmail.com>
2023-04-12 09:51:17 -0400

    added health potion

---
+++ Mythology Mayhem/Assets/HealingPotion.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPotion : MonoBehaviour
+{
+    [SerializeField] private PlayerStats player;
+    [SerializeField] private bool smallPotion;
+
+    [SerializeField] private float amplitude = 0.5f;
+    [SerializeField] private float frequency = 1f;
+
+    private Vector2 posOffset = new Vector2();
+    private Vector2 tempPos = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerStats>();
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.layer == 3)
+        {
+            if (smallPotion) 
+                other.gameObject.GetComponent<PlayerStats>().Heal(2);
+            else
+                other.gameObject.GetComponent<PlayerStats>().Heal(4);
+            Destroy(gameObject);
+        }  
+    }
+}
