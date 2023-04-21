78b9e8dc1da10dd4aca7e1570dad6be175134f29
tyhan216 <60100386+tyhan216@users.noreply.github.com>
2023-04-21 17:01:36 -0400

    Item_Pickup
    Item pickup
    and adding items to the 2d scene

--- Mythology Mayhem/Assets/SceneSwitch.cs
+++ Mythology Mayhem/Assets/SceneSwitch.cs
@@ -8,17 +8,60 @@ public class SceneSwitch : MonoBehaviour
     public string sceneName;
     public float distance;
     Transform player;
+    itemSlot itemSlot;
+    //SaveScene saveScene;
     
-    void Awake()
+    void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        itemSlot = FindObjectOfType<itemSlot>();
+        //saveScene = GameObject.FindGameObjectWithTag("SaveScene").GetComponent<SaveScene>();
     }
 
     void Update()
     {
         if(Vector3.Distance(player.position, transform.position) < distance)
         {
-            SceneManager.LoadScene(sceneName);
+            print("Right Disatnce: " + itemSlot.gameObject.activeSelf);
+            if(itemSlot.item != null)
+            {
+                if(itemSlot.gameObject != null)
+                {
+                    // foreach (var item in saveScene.sceneObjects.objects)
+                    // {
+                    //     if(item.gameObject == itemSlot.gameObject)
+                    //     {
+                    //         item.isEnabled = false;
+                    //     }
+                    // }
+                    // saveScene.sceneObjects.objects[IndexOfScene(itemSlot.gameObject)].isEnabled = false;
+                    // //Destroy(itemSlot.gameObject);
+                    // itemSlot.NotSave();
+
+                    PlayerPrefs.SetString("TranstionItem", itemSlot.item.name);
+                    print(PlayerPrefs.GetString("TranstionItem"));
+
+                    SceneManager.LoadScene(sceneName);  
+                }
+            }
+            
         }
+    }
+
+    public int IndexOfScene(GameObject gameObject)
+    {
+        int index = 0;
+        // foreach (var item in saveScene.sceneObjects.objects)
+        // {
+        //     if(item.gameObject == gameObject)
+        //     {
+        //         index = saveScene.sceneObjects.objects.IndexOf(item);
+        //     }
+        // }
+        return index;
+    }
+    void LateUpdate()
+    {
+        
     }
 }
