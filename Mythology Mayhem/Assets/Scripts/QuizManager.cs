6d5eef88e0317c7b23a72aab8bfeea0cd3e30955
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2023-05-26 01:59:00 -0400

    fixed extra screen issue with quiz manager

--- Mythology Mayhem/Assets/Scripts/QuizManager.cs
+++ Mythology Mayhem/Assets/Scripts/QuizManager.cs
@@ -9,6 +9,7 @@ public class QuizManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI answer1;
     [SerializeField] TextMeshProUGUI answer2;
     [SerializeField] TextMeshProUGUI answer3;
+    [SerializeField] TextMeshProUGUI answer4;
     [SerializeField] Questions[] allQuestions;
     [TextArea(3,7)]
     [SerializeField] string introduction;
@@ -25,30 +26,46 @@ public class QuizManager : MonoBehaviour
         answer1.text = "Begin";
         answer2.gameObject.transform.parent.transform.gameObject.SetActive(false);
         answer3.gameObject.transform.parent.transform.gameObject.SetActive(false);
+        answer4.gameObject.transform.parent.transform.gameObject.SetActive(false);
     }//end start
 
     void DisplayQuestion() {
-        question.text = allQuestions[currentQuestion].questionText;
-        answer1.text = allQuestions[currentQuestion].answers[0];
-        answer2.text = allQuestions[currentQuestion].answers[1];
-        answer3.text = allQuestions[currentQuestion].answers[2];        
+        if(currentQuestion < allQuestions.Length) {
+            question.text = allQuestions[currentQuestion].questionText;
+            answer1.text = allQuestions[currentQuestion].answers[0];
+            answer2.text = allQuestions[currentQuestion].answers[1];
+            answer3.text = allQuestions[currentQuestion].answers[2];
+            answer4.text = allQuestions[currentQuestion].answers[3];
+
+            answer2.gameObject.transform.parent.transform.gameObject.SetActive(true);
+            answer3.gameObject.transform.parent.transform.gameObject.SetActive(true);
+            answer4.gameObject.transform.parent.transform.gameObject.SetActive(true);  
+        } else {
+            answered = true;
+        }
+                
     }//end display question
 
     public void AnswerQuestion(int x) {
+
         if(currentQuestion == -1) {
             answer2.gameObject.transform.parent.transform.gameObject.SetActive(true);
             answer3.gameObject.transform.parent.transform.gameObject.SetActive(true);
+            answer4.gameObject.transform.parent.transform.gameObject.SetActive(true);
             currentQuestion++;
             DisplayQuestion();
-        } else if(currentQuestion == allQuestions.Length) {
+        } else if(currentQuestion == allQuestions.Length - 1 & answered) {
             question.text = "Final Score: " + score;
             if(score >= 2) {                
                 answer1.text = "You won a boost!";
             } else {
                 answer1.text = "You lost and bear a curse!";
             }
+            answer2.gameObject.transform.parent.transform.gameObject.SetActive(false);
+            answer3.gameObject.transform.parent.transform.gameObject.SetActive(false);
+            answer4.gameObject.transform.parent.transform.gameObject.SetActive(false);
             currentQuestion++;
-        } else if(currentQuestion >= allQuestions.Length) {
+        } else if(currentQuestion > allQuestions.Length) {
             Destroy(this.gameObject);
         }
         else {
@@ -62,26 +79,29 @@ public class QuizManager : MonoBehaviour
                 answer1.text = "Continue";
                 answer2.gameObject.transform.parent.transform.gameObject.SetActive(false);
                 answer3.gameObject.transform.parent.transform.gameObject.SetActive(false);
+                answer4.gameObject.transform.parent.transform.gameObject.SetActive(false);
                 answered = true;
             }
             else {
                 DelayMovement();
                 answered = false;
-                answer2.gameObject.transform.parent.transform.gameObject.SetActive(true);
-                answer3.gameObject.transform.parent.transform.gameObject.SetActive(true);
             }            
         }        
+
+        if(currentQuestion == allQuestions.Length) {
+            currentQuestion++;
+        }
         
     }//end answer questions
 
     void DelayMovement() {
-        if(currentQuestion < allQuestions.Length - 1) {
+        if(currentQuestion < allQuestions.Length) {
             currentQuestion++;
             DisplayQuestion(); 
         } else {
-            currentQuestion++;
             answer2.gameObject.transform.parent.transform.gameObject.SetActive(false);
             answer3.gameObject.transform.parent.transform.gameObject.SetActive(false);
+            answer4.gameObject.transform.parent.transform.gameObject.SetActive(false);
         }
     }//end delay movement
     
