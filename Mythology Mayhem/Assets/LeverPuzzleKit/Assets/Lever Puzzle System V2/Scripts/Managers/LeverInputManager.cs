bd6b8f0a618ba0bab99d64c046a94a4bdb84c980
Samantha Durfee <88119631+EpicZombs33@users.noreply.github.com>
2023-06-07 16:39:52 -0400

    Revert "levers UNFINISHED"
    This reverts commit f027f0d89ca0e609df4d85228a7b784f53a46749.

---
+++ Mythology Mayhem/Assets/LeverPuzzleKit/Assets/Lever Puzzle System V2/Scripts/Managers/LeverInputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LeverSystem
+{
+    public class LeverInputManager : MonoBehaviour
+    {
+        [Header("Raycast Interact Input")]
+        public KeyCode interactKey;
+
+        public static LeverInputManager instance;
+
+        private void Awake()
+        {
+            if (instance != null) { Destroy(gameObject); }
+            else { instance = this; DontDestroyOnLoad(gameObject); }
+        }
+    }
+}
