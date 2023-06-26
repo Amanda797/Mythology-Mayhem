f7d3ab64a4e4e894596b5c68e9c3c2728a5038d6
Joshua Hixon <83591182+JTHixon@users.noreply.github.com>
2023-06-26 12:26:39 -0400

    They now shoot diagonally. Still working to get arrow drop to work on diagonal shots.

--- Mythology Mayhem/Assets/Scripts/2D/Shoot2D.cs
+++ Mythology Mayhem/Assets/Scripts/2D/Shoot2D.cs
@@ -6,19 +6,22 @@ using UnityEngine.SceneManagement;
 public class Shoot2D : MonoBehaviour
 {
     public GameObject ArrowPrefab;
-    public GameObject ArrowPrefab2;
     public Transform ArrowSpawn;
     public float TBS = 0f;
     private float m_timestamp = 0f;
     public AudioSource source;
     public float AS = 0f;
     public float AL = 0f;
+    public float AL2 = 0f;
     public GameObject player;
     private bool CS = false;
     private float up = 0f;
-    [SerializeField] private Transform Upward;
     public bool CSU = false;
     private bool CSN = false;
+    [SerializeField] private float ASx = 0f;
+    [SerializeField] private float ASy = 0f;
+    [SerializeField] private float AD = 0f;
+    [SerializeField] private float ADi = 0f;
 
     [SerializeField] private Animator anim;
 
@@ -33,12 +36,12 @@ public class Shoot2D : MonoBehaviour
         {
             CS = !CS;
         }
-        if(CS == true)
+        if (CS == true)
         {
             anim.SetBool("CanShoot", true);
             anim.SetBool("UseSword", false);
         }
-        if(CS == false)
+        if (CS == false)
         {
             anim.SetBool("CanShoot", false);
             anim.SetBool("UseSword", true);
@@ -47,18 +50,18 @@ public class Shoot2D : MonoBehaviour
         {
             CS = false;
         }*/
-        if((Time.time >= m_timestamp) && (Input.GetKeyDown(KeyCode.Mouse0)) && player.GetComponent<PlayerController>().pushing == false && CS == true && CSN == true)
+        if ((Time.time >= m_timestamp) && (Input.GetKeyDown(KeyCode.Mouse0)) && player.GetComponent<PlayerController>().pushing == false && CS == true && CSN == true)
         {
             Shoot();
             m_timestamp = Time.time + TBS;
             source.Play();
         }
-        if(up == 1f)
+        if (up == 1f)
         {
             CSU = true;
             anim.SetBool("CanShootUpward", true);
         }
-        else if(up < 1f)
+        else if (up < 1f)
         {
             CSU = false;
             anim.SetBool("CanShootUpward", false);
@@ -67,7 +70,7 @@ public class Shoot2D : MonoBehaviour
         {
             CSN = false;
         }
-        else if(CSU == false)
+        else if (CSU == false)
         {
             CSN = true;
         }
@@ -83,11 +86,11 @@ public class Shoot2D : MonoBehaviour
     {
         anim.SetTrigger("Shoot");
         var arrow = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, ArrowSpawn.rotation);
-        if(gameObject.GetComponent<PlayerStats>().flipped == false)
+        if (gameObject.GetComponent<PlayerStats>().flipped == false)
         {
             arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(AS, 0.0f);
         }
-        else 
+        else
         {
             arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(-AS, 0.0f);
         }
@@ -96,11 +99,18 @@ public class Shoot2D : MonoBehaviour
     public void ShootUp()
     {
         anim.SetTrigger("ShootUpward");
-        var arrow2 = (GameObject)Instantiate(ArrowPrefab2, Upward.position, Upward.rotation);
-        if(up > 0.01f && CSU == true)
+        if (up > 0.01f && CSU == true && gameObject.GetComponent<PlayerStats>().flipped == false)
         {
-            arrow2.GetComponent<Rigidbody2D>().velocity = new Vector2(0, AS);
+            var arrow2 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 0, AD));
+            arrow2.GetComponent<Rigidbody2D>().velocity = new Vector2(ASx, ASy);
+            Destroy(arrow2, AL2);
         }
-        Destroy(arrow2, AL);
+        if (up > 0.01f && CSU == true && gameObject.GetComponent<PlayerStats>().flipped == true)
+        {
+            var arrow2 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 0, ADi));
+            arrow2.GetComponent<Rigidbody2D>().velocity = new Vector2(-ASx, ASy);
+            Destroy(arrow2, AL2);
+        }
+        //Destroy(arrow2, AL);
     }
 }
