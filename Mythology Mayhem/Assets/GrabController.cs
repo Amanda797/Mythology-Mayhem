68a8af04538b3e9c98d7a394ef0348990a7c0092
Samantha Durfee <88119631+EpicZombs33@users.noreply.github.com>
2023-06-29 16:22:37 -0400

    pedestals puzzle push
    push what work i have done on the peds puzzle incomplete still working on grabbing objects and inventory system

---
+++ Mythology Mayhem/Assets/GrabController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+
+        if (grabCheck.collider != null && grabCheck.collider.tag == "Box")
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+            else
+            {
+                grabCheck.collider.gameObject.transform.parent = null;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmos() 
+{
+    Gizmos.color = Color.green;
+
+    Gizmos.DrawLine(transform.position,transform.position + Vector3.right * transform.localScale.x * rayDist);
+}
+}
