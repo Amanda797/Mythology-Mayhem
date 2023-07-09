e2e7852dea7846bc189acc35945f53665bdd54c3
Samantha Durfee <88119631+EpicZombs33@users.noreply.github.com>
2023-07-09 14:40:59 -0400

    "pushing"

--- Mythology Mayhem/Assets/Scripts/3D/CollectItem.cs
+++ Mythology Mayhem/Assets/Scripts/3D/CollectItem.cs
@@ -7,17 +7,11 @@ public class CollectItem : MonoBehaviour
     private void OnTriggerEnter(Collider other) 
     {
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
-        
 
         if (playerInventory != null )
         {
             playerInventory.EarthCollect();
             gameObject.SetActive(false);
-
-            //PedPuzzleItems elements = GetComponent<earth1>();
-            //earth1 = true;
-            //PedastalsPuzzleManager earth = GetComponent<apple>();
-            //apple = true;
         }
     }
 }
