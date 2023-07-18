97f283967c0885bd9ed4822fcfa186291b63674f
SL33P3RHLD <sl33p3rhld@gmail.com>
2023-07-18 18:54:20 -0400

    Setting up Remainder of Medusa State Machine
    Rounded out state machine for the boss fight. Add in basic effects for attacks, aggro types, progressive variables for increasing difficulty as I went.

---
+++ Mythology Mayhem/Assets/Medusa/Scripts/MedusaSpellAttackScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedusaSpellAttackScript : MonoBehaviour
+{
+
+    public float speed;
+
+    public GameObject damageSphere;
+
+    public float startScale;
+    public float endScale;
+    public float timeWhenHit;
+    public float timeToScale;
+    public float timeReachedFullScale;
+    public bool fullScale;
+    public float timeAtFullScale;
+
+
+    public bool stopMovement;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        damageSphere.SetActive(false);
+        fullScale = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb != null && !stopMovement) 
+        {
+            rb.velocity = transform.forward * speed * Time.deltaTime;
+        }
+        if (stopMovement)
+        {
+            float scaleAmount = (Time.time - timeWhenHit) / timeToScale;
+            if (scaleAmount >= 1 && !fullScale)
+            {
+                scaleAmount = 1;
+                fullScale = true;
+                timeReachedFullScale = Time.time;
+            }
+
+            float finalAmount = Mathf.Lerp(startScale, endScale, scaleAmount);
+            damageSphere.transform.localScale = new Vector3(finalAmount, finalAmount, finalAmount);
+            if (Time.time - timeReachedFullScale >= timeAtFullScale && fullScale)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Ground")
+        {
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                stopMovement = true;
+                damageSphere.transform.localScale = new Vector3(startScale, startScale, startScale);
+                damageSphere.SetActive(true);
+                timeWhenHit = Time.time;
+            }
+        }
+    }
+}
