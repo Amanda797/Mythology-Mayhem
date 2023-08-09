503228422f1e6ac61286f13eb23080395d3e62f3
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2023-08-09 00:55:49 -0400

    Start of AudioRandomizer Scruitable Objects
    For randomizing audio clips and eventually blending sounds

---
+++ Mythology Mayhem/Assets/Global Assets/Audio/AudioRandomizer_SO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioRandomizer_SO", menuName = "AudioRandomizer_SO", order = 1)]
+
+public class AudioRandomizer_SO : ScriptableObject
+{
+    public List<AudioClipGroup> audioClips = new();
+
+    public AudioClip PlaySiblings() {
+        int randomClip = Random.Range(0, audioClips.Count);
+        return audioClips[randomClip].mainAudioClip;
+    }
+}
+
+[System.Serializable]
+public class AudioClipGroup {
+    public AudioClip mainAudioClip;
+    public AudioClipGroup parentAudioClip;
+    public List<AudioClipGroup> childrenAudioClips;
+}
