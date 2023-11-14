0f9a103a7efbc8ac1edd00af5d7e2dfc64e153e0
SL33P3RHLD <sl33p3rhld@gmail.com>
2023-11-14 16:16:36 -0500

    Added Marble Viking, Fixed barrel and crate not rending (lost mesh data, stole from snow versions) Setup Animator and base code for Marble

---
+++ Mythology Mayhem/Assets/3D Assets/Norse/Village3D/9MM/Scripts/NineMMAnimScript.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NineMMAnimScript : MonoBehaviour
+{
+    public int currentMove;
+    public Animator anim;
+    public Animator vikingAnim;
+
+    public GameObject[] marblePickups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMove = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetMove(int move)
+    {
+        currentMove = move;
+    }
+
+    public void RunMove()
+    {
+        anim.SetInteger("Move", currentMove);
+        switch (currentMove)
+        {
+            case 0:
+                vikingAnim.SetBool("Playing", true);
+                break;
+            case 1:
+                vikingAnim.SetTrigger("LowerHead");
+                break;
+            case 2:
+                vikingAnim.SetTrigger("Cheer");
+                break;
+            case 3:
+                vikingAnim.SetTrigger("LowerHead");
+                break;
+            case 4:
+                vikingAnim.SetTrigger("Move");
+                break;
+            case 5:
+                vikingAnim.SetTrigger("Loss");
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            RunMove();
+            ActivateNextPickup(currentMove + 1);
+        }
+    }
+
+    void ActivateNextPickup(int next)
+    {
+        if (next <= marblePickups.Length)
+        {
+            for (int i = 0; i < marblePickups.Length; i++)
+            {
+                if (marblePickups[i] != null)
+                {
+                    if (i == next - 1)
+                    {
+                        marblePickups[i].SetActive(true);
+                    }
+                    else
+                    {
+                        marblePickups[i].SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
