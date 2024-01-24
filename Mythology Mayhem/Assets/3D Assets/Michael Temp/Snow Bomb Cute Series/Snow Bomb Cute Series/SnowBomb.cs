7f32dbb59cd1324e4bd4db0f9745cf8259943d5c
VargentSomnium <52584900+VargentSomnium@users.noreply.github.com>
2024-01-24 13:46:31 -0500

    Snow Bomb Prefab
    Created prefab
    -Outlined main components
    -used existing script before modifying for explode attack
    -navmesh agent as flying

---
+++ Mythology Mayhem/Assets/3D Assets/Michael Temp/Snow Bomb Cute Series/Snow Bomb Cute Series/SnowBomb.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBomb : MonoBehaviour
+{
+    Enemy enemy;
+
+    [Header("Idle & Patrol")]
+    [SerializeField] string walkBool;
+    [SerializeField] string runBool;
+    [SerializeField] float patrolDistance = 30f;
+
+    [Header("Melee Attack")]
+    [SerializeField] GameObject body;
+    [SerializeField] string meleeAttackTrigger;
+    [SerializeField] float meleeDistance = 10f;
+
+    /*[Header("Taunt Attack")]
+    [SerializeField] GameObject summonViking;
+    [SerializeField] bool summonUsed;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = gameObject.GetComponent<Enemy>();
+    }
+
+    bool idleTransition = false;
+
+    public void Idle()
+    {
+        //Check for Player
+        if (enemy.DetectPlayer())
+        {
+            StartCoroutine(enemy.SwitchStates(Enemy.EnemyStates.Attack, 0));
+        }
+
+        // Continue Idle
+        if (enemy.idleTimer <= 0 && !idleTransition)
+        {
+            idleTransition = true;
+            enemy.agent.isStopped = false;
+            enemy.animator.SetBool(walkBool, true);
+            StartCoroutine(enemy.SwitchStates(Enemy.EnemyStates.Patrol, 0));
+        }
+        else
+        {
+            idleTransition = false;
+            enemy.agent.isStopped = true;
+            enemy.idleTimer -= Time.deltaTime;
+            enemy.animator.SetBool(walkBool, false);
+        }
+    }//end move to target
+
+    public void MoveToTarget()
+    {
+        //Check for Player
+        if (enemy.DetectPlayer())
+        {
+            StartCoroutine(enemy.SwitchStates(Enemy.EnemyStates.Attack, 0));
+        }
+
+        // Continue M2T
+        if (Vector3.Distance(enemy.gameObject.transform.position, enemy.target) < 3f)
+        {
+            //Close enough to Idle
+            StartCoroutine(enemy.SwitchStates(Enemy.EnemyStates.Idle, 0));
+        }
+        else
+        {
+            enemy.agent.isStopped = false;
+            enemy.agent.SetDestination(enemy.target);
+        }
+    }//end move to target
+
+    public void MeleeAttack()
+    {
+        //Check for Player
+        if (!enemy.DetectPlayer())
+        {
+            StartCoroutine(enemy.SwitchStates(Enemy.EnemyStates.Patrol, 0));
+        }
+
+        // Continue Attack
+        if (Vector3.Distance(body.transform.position, enemy.player.transform.position) < meleeDistance && enemy.CanAttack)
+        {
+            enemy.agent.isStopped = true;
+            enemy.animator.SetTrigger(meleeAttackTrigger);
+            enemy.player.GetComponent<FPSHealth>().TakeDamage(enemy.attackDamage);
+            if (enemy.player.GetComponent<KnockBackFeedback>())
+                enemy.player.GetComponent<KnockBackFeedback>().PlayerFeedback(gameObject);
+            enemy.CanAttack = false;
+            StartCoroutine(enemy.AttackRate());
+        }
+        else
+        {
+            enemy.agent.isStopped = false;
+            enemy.agent.SetDestination(enemy.player.transform.position);
+            enemy.agent.speed = enemy.walkSpeed;
+            enemy.animator.SetBool(runBool, true);
+        }
+    }//end melee attack
+}
