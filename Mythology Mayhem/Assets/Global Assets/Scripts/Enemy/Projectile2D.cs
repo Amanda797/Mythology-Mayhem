43f205195b4555cd92a16938cd87e566db6e5985
JeremyClark-McKay <JBClarkMcKay@student.fullsail.edu>
2024-02-16 11:35:41 -0600

    Early commit of projectile toss to do QA

---
+++ Mythology Mayhem/Assets/Global Assets/Scripts/Enemy/Projectile2D.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+
+    [SerializeField]  float Speed;
+    [SerializeField]  int Damage;
+    [SerializeField] float _destroyTimer;
+    public float DestroyTimer { get => _destroyTimer; set => _destroyTimer = value; }
+    private float DestroyCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyCount = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestroyCount += Time.deltaTime;
+        if(DestroyCount >= DestroyTimer)
+        {
+            Destroy(gameObject);
+        }
+        transform.position += -transform.right * Time.deltaTime * Speed;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Player")){
+            Destroy(gameObject);
+            collision.gameObject.GetComponent<FPSHealth>().TakeDamage(Damage);
+        }
+    }
+}
