69f7554b76331bb3ad4a46ee89dd9016596caf8e
SL33P3RHLD <sl33p3rhld@gmail.com>
2024-02-15 21:53:14 -0500

    Bugs Fixes

---
+++ Mythology Mayhem/Assets/Global Assets/Scripts/UI/GameplayUILink.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayUILink : MonoBehaviour
+{
+
+    public GameObject scrollPanel;
+    public TMP_Text scrollDisplayText;
+    public GameObject pressEText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
