1041742bc2233ece890e8bdce907ec4f969fa2f6
Gausroth <66494221+Gausroth@users.noreply.github.com>
2024-08-05 14:12:00 -0500

    Scroll SFX
    3D scrolls now play an SFX when opened.
    2D and 3D scrolls have a new SFX.

--- Mythology Mayhem/Assets/Global Assets/Scripts/ScrollPopup.cs
+++ Mythology Mayhem/Assets/Global Assets/Scripts/ScrollPopup.cs
@@ -5,46 +5,39 @@ using UnityEngine;
 public class ScrollPopup : MonoBehaviour
 {
     GameManager gameManager;
-    public KeyCode key;
+    AudioSource audioSource;
+
+    // Hover Vars
     public float activeDistance = 6f;
     public float hoverDistance;
     float hover;
+
+
     public GameplayUILink gameplayUI;
     public GameObject popup;
     public TMP_Text textMeshObject;
-    public GameObject pressText;
     public GameObject supriseObject;
     public bool isPopupActive = false;
     public bool surpriseSpawned = false;
     [TextArea(4, 9)]
     public string scrollText;
-
-
-    bool activeStatus = false;
-    GameObject player = null;
+    bool canRead = false;
 
     void Start()
     {
-        // try to find the GameManager object
-        if (GameManager.instance != null)
-        {
-            // if found set variable(s)
-            gameManager = GameManager.instance;
-        }
-        // else display a warning that it is missing
+        if (GameManager.instance != null) gameManager = GameManager.instance;
         else Debug.LogWarning("GameManager Missing or Inactive.");
 
-        // try to find the GameplayUILink object
         if (FindObjectOfType<GameplayUILink>() != null)
         {
-            // if found set variable(s)
             gameplayUI = FindObjectOfType<GameplayUILink>();
             popup = gameplayUI.scrollPanel;
             textMeshObject = gameplayUI.scrollDisplayText;
-            pressText = gameplayUI.pressEText;
         }
-        // else display a warning that it is missing
         else Debug.LogWarning("GameplayUILink Missing or Inactive.");
+
+        if (GetComponent<AudioSource>() != null) audioSource = GetComponent<AudioSource>();
+        else Debug.LogWarning("AudioSource Missing or Inactive.");
 
         hover = hoverDistance;
 
@@ -52,22 +45,22 @@ public class ScrollPopup : MonoBehaviour
     }
     void Update()
     {
-        if (!activeStatus) return;
+        if (!canRead) return;
+        if (Time.timeScale == 0) return;
 
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (isPopupActive)
+            textMeshObject.text = scrollText;
+            popup.SetActive(true);
+            hover = 0;
+            audioSource.Play();
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 0;
+
+            if (supriseObject != null && !surpriseSpawned)
             {
-                HidePopup();
-                isPopupActive = false;
-            }
-            else
-            {
-                ShowPopup();
-                //StopCoroutine(Hover());
-                isPopupActive = true;
-                if (supriseObject != null && !surpriseSpawned)
-                    supriseObject.SetActive(true);
+                supriseObject.SetActive(true);
                 surpriseSpawned = true;
             }
         }
@@ -79,30 +72,8 @@ public class ScrollPopup : MonoBehaviour
         {
             // activate and display text on the popup ui
             gameManager.Popup("Press E to Read", true);
-            activeStatus = true;
-            player = other.gameObject;
+            canRead = true;
         }
-        //if (!other.gameObject.CompareTag("Player")) return;
-
-        //if (GameManager.instance != null) GameManager.instance.Popup("Press E to Read");
-
-        //if (Input.GetKeyDown(key))
-        //{
-        //    if (isPopupActive)
-        //    {
-        //        HidePopup();
-        //        isPopupActive = false;
-        //    }
-        //    else
-        //    {
-        //        ShowPopup();
-        //        //StopCoroutine(Hover());
-        //        isPopupActive = true;
-        //        if (supriseObject != null && !surpriseSpawned)
-        //            supriseObject.SetActive(true);
-        //        surpriseSpawned = true;
-        //    }
-        //}
     }
 
     private void OnTriggerExit(Collider other)
@@ -110,29 +81,9 @@ public class ScrollPopup : MonoBehaviour
         // if the object entering the trigger is the player
         if (other.gameObject.tag == "Player")
         {
-            // deactivate the popup ui
             gameManager.Popup("Press E to Read", false);
-            activeStatus = false;
-            player = null;
+            canRead = false;
         }
-    }
-    public void ShowPopup()
-    {
-        textMeshObject.text = scrollText;
-        popup.SetActive(true);
-        hover = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.Confined;
-        Time.timeScale = 0;
-    }
-
-    public void HidePopup()
-    {
-        popup.SetActive(false);
-        hover = hoverDistance;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        Time.timeScale = 1;
     }
 
     // Ienumerator that makes the scroll hover up and down with lerping
