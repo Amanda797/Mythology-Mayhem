6f5322c5251010ead079dd4cab3a22a0859c3ae9
Dequan <demilete@student.fullsail.edu>
2024-08-06 17:23:41 -0400

    camera fixes

--- Mythology Mayhem/Assets/Global Assets/Scripts/CameraFollow2D.cs
+++ Mythology Mayhem/Assets/Global Assets/Scripts/CameraFollow2D.cs
@@ -8,7 +8,7 @@ public class CameraFollow2D : MonoBehaviour
     public Camera camera2D;
     public GameObject target;
     public float buffer;
-    //public float height;
+    public float height;
     Vector3 start;
 
     // Start is called before the first frame update
@@ -18,7 +18,13 @@ public class CameraFollow2D : MonoBehaviour
         else Debug.LogWarning("GameManager Missing.");
 
         start = transform.position;
+        height = target.transform.position.y + 12;
         Debug.Log(start.y);
+    }
+
+    private void OnEnable()
+    {
+        height = target.transform.position.y + 12;
     }
 
     // Update is called once per frame
@@ -30,7 +36,8 @@ public class CameraFollow2D : MonoBehaviour
             Transform[] border = gameManager.currentLocalManager.boundaries;
             float left = Mathf.Min(border[0].position.x, border[1].position.x) + camera2D.orthographicSize * camera2D.aspect + buffer;
             float right = Mathf.Max(border[0].position.x, border[1].position.x) - camera2D.orthographicSize * camera2D.aspect - buffer;
-            transform.position = new Vector3(Mathf.Clamp(target.transform.position.x, left, right), start.y, transform.position.z);
+            height = Mathf.Min(target.transform.position.y + 4, height);
+            transform.position = new Vector3(Mathf.Clamp(target.transform.position.x, left, right), height, transform.position.z);
         }
         else
             transform.position = new Vector3(transform.position.x, start.y, transform.position.z);
