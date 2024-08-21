2f4d5ee1f249cae0e02d305269b02d4234c4bc42
Gausroth <66494221+Gausroth@users.noreply.github.com>
2024-08-21 11:42:02 -0500

    2D Bow Attack
    Simplified the Shoot2D script.
    The 2D bow is now activated by pressing 1.
    The 2D bow will not work if the game is paused, the player has not collected the bow or of the player is pushing or climbing.
    Cleaned up the 2D arrow .png.
    Fixed the 2D arrow box collider.
    The 2D arrow will no longer collide with the player.
    Fixed a bug that prevented the sword animation from playing after switching to the bow and back to the sword.
    Fixed a bug with the 2D bird collider.

--- Mythology Mayhem/Assets/2D Assets/Global/Scripts/2D/Shoot2D.cs
+++ Mythology Mayhem/Assets/2D Assets/Global/Scripts/2D/Shoot2D.cs
@@ -5,161 +5,116 @@ using UnityEngine.SceneManagement;
 
 public class Shoot2D : MonoBehaviour
 {
+    GameManager gameManager;
+    PlayerController playerController;
+    PlayerStats playerStats;
+    Animator anim;
+
+    public PlayerStats_SO ps;
+    public AudioSource audioSource;
     public GameObject ArrowPrefab;
     public Transform ArrowSpawn;
-    public float TBS = 0f;
+
     private float m_timestamp = 0f;
-    public AudioSource source;
     public float AS = 0f;
     public float AL = 0f;
     public float AL2 = 0f;
-    public GameObject player;
-    private bool CS = false;
-    private float up = 0f;
-    private bool CSU = false;
-    private bool CSN = false;
+    private bool canShoot = false;
+    private float aim = 0f;
+    private bool canShootUp = false;
+    private bool canShootDown = false;
+    private bool canShootAhead = false;
     [SerializeField] private float ASx = 0f;
     [SerializeField] private float ASy = 0f;
     [SerializeField] private float AD = 0f;
     [SerializeField] private float ADi = 0f;
-    private bool CSD = false;
 
-    [SerializeField] private Animator anim;
-    [SerializeField] public PlayerStats_SO ps;
 
-    void Start()
+    private void Awake()
     {
-        CS = false;
-
-        HealthUIController huic = GameObject.FindGameObjectWithTag("huic").GetComponent<HealthUIController>();
-        if(huic != null) { 
-            ps = huic.ps;
-            ps.CanAttack = true;
-            ps.NextAttackTime = 0;
-            //ps.CurrHealth = ps.MaxHealth;
-        }
-        else{
-            print("Can't find huic's player stats so");
-        }
+        playerController = GetComponent<PlayerController>();
+        playerStats = GetComponent<PlayerStats>();
+        anim = GetComponent<Animator>();
+        gameManager = GameManager.instance;
     }
     void Update()
     {
-        up = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.X) && CS == true || Input.GetKeyDown(KeyCode.X) && CS == false)
-        {
-            CS = !CS;
-        }
-        if (CS == true)
-        {
-            anim.SetBool("CanShoot", true);
-            anim.SetBool("UseSword", false);
+        if (playerController.pushing) return;
+        if (playerController.climbing) return;
+        if (!gameManager.gameData.collectedBow) return;
+        if (Time.timeScale != 1) return;
 
-            ps.CanAttack = false;
-        }
-        if (CS == false)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            anim.SetBool("CanShoot", false);
-            anim.SetBool("UseSword", true);
+            canShoot = !canShoot;
 
-            ps.CanAttack = true;
+            anim.SetBool("CanShoot", canShoot);
+            anim.SetBool("UseSword", !canShoot);
+
+            ps.CanAttack = !canShoot;
         }
-        /*if(SceneManager.GetActiveScene().name == "2Dlabyrinth_Levers 1" || SceneManager.GetActiveScene().name == "2Dlabyrinth_Pedastals")
-        {
-            CS = false;
-        }*/
-        if ((Time.time >= m_timestamp) && (Input.GetKeyDown(KeyCode.Mouse0)) && player.GetComponent<PlayerController>().pushing == false && CS == true && CSN == true)
+
+        if (!canShoot) return;
+
+        aim = Input.GetAxisRaw("Vertical");
+
+        canShootUp = aim > 0;
+        canShootDown = aim < 0;
+        canShootAhead = aim == 0;
+
+        anim.SetBool("CanShootUpward", canShootUp);
+        anim.SetBool("CanShootDownward", canShootDown);
+
+        if (Time.time >= m_timestamp && Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot();
-            m_timestamp = Time.time + TBS;
-            source.Play();
-        }
-        if (up == 1f)
-        {
-            CSU = true;
-            CSD = false;
-            anim.SetBool("CanShootUpward", true);
-        }
-        else if (up == 0f)
-        {
-            CSU = false;
-            CSD = false;
-            anim.SetBool("CanShootUpward", false);
-            anim.SetBool("CanShootDownward", false);
-        }
-        if(up == -1f)
-        {
-            CSD = true;
-            CSU = false;
-            anim.SetBool("CanShootDownward", true);
-        }
-        if (CSU == true || CSD == true)
-        {
-            CSN = false;
-        }
-        else if (CSU == false || CSD == false)
-        {
-            CSN = true;
-        }
-        if ((Time.time >= m_timestamp) && (Input.GetKeyDown(KeyCode.Mouse0)) && player.GetComponent<PlayerController>().pushing == false && CS == true && CSU == true && CSN == false && CSD == false)
-        {
-            ShootUp();
-            m_timestamp = Time.time + TBS;
-            source.Play();
-        }
-        if ((Time.time >= m_timestamp) && (Input.GetKeyDown(KeyCode.Mouse0)) && player.GetComponent<PlayerController>().pushing == false && CS == true && CSD == true)
-        {
-            ShootDown();
-            m_timestamp = Time.time + TBS;
-            source.Play();
+            m_timestamp += Time.deltaTime;
+            audioSource.Play();
         }
     }
 
     public void Shoot()
     {
-        anim.SetTrigger("Shoot");
-        if (gameObject.GetComponent<PlayerStats>().flipped == false)
+        GameObject arrow = Instantiate(ArrowPrefab, ArrowSpawn.position, ArrowSpawn.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+
+        if (aim == 0)
         {
-            var arrow = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, ArrowSpawn.rotation);
-            arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(AS, 0.0f);
+            anim.SetTrigger("Shoot");
             Destroy(arrow, AL);
+
+            if (playerStats.flipped)
+            {
+                arrow.transform.rotation = Quaternion.Euler(0, 180, 0);
+                rb.velocity = new Vector2(-AS, 0.0f);
+            }
+            else rb.velocity = new Vector2(AS, 0.0f);
         }
-        else
+        else if (aim > 0)
         {
-            var arrow = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 180, 0));
-            arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(-AS, 0.0f);
-            Destroy(arrow, AL);
+            anim.SetTrigger("ShootUpward");
+            arrow.transform.rotation = Quaternion.Euler(0, 0, AD);
+            Destroy(arrow, AL2);
+
+            if (playerStats.flipped)
+            {
+                arrow.transform.rotation = Quaternion.Euler(0, 180, AD);
+                rb.velocity = new Vector2(-ASx, ASy);
+            }
+            else rb.velocity = new Vector2(ASx, ASy);
         }
-    }
-    public void ShootUp()
-    {
-        anim.SetTrigger("ShootUpward");
-        if (up > 0.01f && CSU == true && gameObject.GetComponent<PlayerStats>().flipped == false && CSD == false)
+        else if (aim < 0)
         {
-            var arrow2 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 0, AD));
-            arrow2.GetComponent<Rigidbody2D>().velocity = new Vector2(ASx, ASy);
-            Destroy(arrow2, AL2);
-        }
-        if (up > 0.01f && CSU == true && gameObject.GetComponent<PlayerStats>().flipped == true && CSD == false)
-        {
-            var arrow2 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 180, AD));
-            arrow2.GetComponent<Rigidbody2D>().velocity = new Vector2(-ASx, ASy);
-            Destroy(arrow2, AL2);
-        }
-    }
-    public void ShootDown()
-    {
-        anim.SetTrigger("ShootDownward");
-        if (up < 0f && CSU == false && gameObject.GetComponent<PlayerStats>().flipped == false && CSD == true)
-        {
-            var arrow3 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 0, ADi));
-            arrow3.GetComponent<Rigidbody2D>().velocity = new Vector2(ASx, -ASy);
-            Destroy(arrow3, AL2);
-        }
-        if (up < 0f && CSU == false && gameObject.GetComponent<PlayerStats>().flipped == true && CSD == true)
-        {
-            var arrow3 = (GameObject)Instantiate(ArrowPrefab, ArrowSpawn.position, Quaternion.Euler(0, 180, ADi));
-            arrow3.GetComponent<Rigidbody2D>().velocity = new Vector2(-ASx, -ASy);
-            Destroy(arrow3, AL2);
+            anim.SetTrigger("ShootDownward");
+            arrow.transform.rotation = Quaternion.Euler(0, 0, ADi);
+            Destroy(arrow, AL2);
+
+            if (playerStats.flipped)
+            {
+                arrow.transform.rotation = Quaternion.Euler(0, 180, ADi);
+                rb.velocity = new Vector2(-ASx, -ASy);
+            }
+            else rb.velocity = new Vector2(ASx, -ASy);
         }
     }
 }
