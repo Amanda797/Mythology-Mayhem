508df92e5bc1d6b966643d0bd072eb5eefe0852d
Gausroth <66494221+Gausroth@users.noreply.github.com>
2024-09-08 13:10:29 -0500

    Removed unused scenes from build

--- Mythology Mayhem/Assets/Global Assets/Scripts/SceneSwitch.cs
+++ Mythology Mayhem/Assets/Global Assets/Scripts/SceneSwitch.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
